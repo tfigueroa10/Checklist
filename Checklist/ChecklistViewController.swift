a9d2bddd32cb421bd31f4ea7fc6a16ba256fb436
Tim Figueroa <tfigueroa10@gmail.com>
2016-12-26 13:40:11 -0700

    test comment

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -5,8 +5,6 @@
 //  Created by Tim Figueroa on 12/22/16.
 //  Copyright © 2016 Tim Figueroa. All rights reserved.
 //
-//  Testing 2
-
 
 import UIKit
 
@@ -22,13 +20,40 @@ class CheckListViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-     func _tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
-     func _tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+       
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row % 5 == 0 {
+            label.text = "Walk the dog please"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Brush your teeth please"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Learn iOS developement using Swift"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Be sure to practice Swift"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "May I have some ice cream please"
+        }
         return cell
     }
 
