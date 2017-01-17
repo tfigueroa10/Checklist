80c20432cbee1d0a98b9de7918dd75d450d4fc4f
Tim Figueroa <tfigueroa10@gmail.com>
2017-01-17 13:55:37 -0700

    Updated the Done action and assure it will close the view at select of Done

--- Checklist/AddItemController.swift
+++ Checklist/AddItemController.swift
@@ -12,18 +12,27 @@ import UIKit
 
 
 class AddItemViewController: UITableViewController {
-    @IBOutlet weak var textField: UITextField!
+   
     @IBAction func cancel() {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBOutlet weak var textField: UITextField!
+    
     @IBAction func done() {
-        print("Contents of the text field: \(textField.text!)")
+        
+    print("Contents of the text field: \(textField.text!)")
 
-        dismiss(animated: true, completion: nil)
+    dismiss(animated: true, completion: nil)
     }
     
-     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-        return nil
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+    return nil
     }
+    
+    override func viewWillAppear(_ animated: Bool){
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
 }
