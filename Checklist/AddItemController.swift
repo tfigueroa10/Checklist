6673a6086f6450a9fdcb7ba90ca299da853580ab
Tim Figueroa <tfigueroa10@gmail.com>
2017-02-27 11:20:48 -0700

    final changes

--- Checklist/AddItemController.swift
+++ Checklist/AddItemController.swift
@@ -12,43 +12,52 @@ import UIKit
 
 
 
+protocol AddItemViewControllerDelegate: class {
+    func addItemViewControllerDidCancel(_ controller: AddItemViewController)
+    func addItemViewController(_ controller: AddItemViewController,
+                               didFinishAdding item: ChecklistItem)
+}
+
 class AddItemViewController: UITableViewController, UITextFieldDelegate {
-   
-    @IBAction func cancel() {
-        dismiss(animated: true, completion: nil)
-    }
     
     @IBOutlet weak var textField: UITextField!
-    
     @IBOutlet weak var doneBarButton: UIBarButtonItem!
-    @IBAction func done() {
-        
-    print("Contents of the text field: \(textField.text!)")
-        
-    dismiss(animated: true, completion: nil)
-    }
     
-    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-    return nil
-    }
+    weak var delegate: AddItemViewControllerDelegate?
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         textField.becomeFirstResponder()
     }
     
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        let oldText = textField.text! as NSString
-        let newText = oldText.replacingCharacters(in: range, with: string)
-        
-        if newText.length > 0 {
-            doneBarButton.isEnabled = true
-        } else {
-            doneBarButton.isEnabled = false
-        }
-        return true
+    @IBAction func cancel() {
+        delegate?.addItemViewControllerDidCancel(self)
     }
+    
+    @IBAction func done() {
+        let item = ChecklistItem()
+        item.text = textField.text!
+        item.checked = false
+        
+        delegate?.addItemViewController(self, didFinishAdding: item)
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+   
 }
 
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        
+        let oldText = textField.text! as NSString
+        let newText = oldText.replacingCharacters(in: range, with: string) as NSString
+        
+        doneBarButton.isEnabled = (newText.length > 0)
+        return true
+   }
+}
 
